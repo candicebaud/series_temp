e1870985914c531a4c17d9d638f121eff1f1363c
candicebaud <candice.baud@epfl.ch>
2023-03-21 20:33:06 +0100

    ça avance

--- project_code.R
+++ project_code.R
@@ -1,41 +1,58 @@
-setwd("C:/Users/candi/Desktop/ETUDES/ENSAE2A/semestre 2/séries temporelles/series temp")
-
 require(zoo)
 require(tseries)
 library(dplyr)
 
-data <- read.csv('valeurs_mensuelles.csv', sep=";")
+#base de données
+data <- read.csv('valeurs_mensuelles_pesticides.csv', sep=";")
 data <- data[2]
-
 
 data <- as.data.frame(data[-(1:3),])#on enlève les premières lignes qui ne sont pas des données
 indice <- as.data.frame(as.numeric(unlist(data)))
 
 xm.source <- zoo(indice[[1]]) #convertit le premier element de data en serie temporelle de type "zoo"
 T <- length(xm.source)
-xm <- xm.source[1:(T-4)]
+xm <- xm.source[1:(T-100)] #pour faire de la prévision
 
-#plot(xm, ylim = 100)
-plot(xm, xaxt="n", ylim= 100) #plot des données
-axis(side=1,at=seq(0,244,12)) #mettre les axes
+plot(xm, xaxt="n") #plot des données
+axis(side=1,at=seq(0,300,12)) #pour mettre l'axe x
 
-xm <- xm - mean(xm) #pour enlever la moyenne
+#xm <- xm - mean(xm) #pour enlever la moyenne
 par(mfrow=c(1,2))
 acf(xm)
-pacf(xm) #pas de saisonnalité apparente
+pacf(xm) #saisonnalité apparente : saisonnalité de 12 donc annuelle
 dev.off()
 
-pp.test(xm) #test de philippe perron ok, on rejette à 1% l'hypothèse que la série n'est pas stationnaire
+#on enlève la saisonnalité apparente
+xm <- diff(xm, lag = 12)
+plot(xm, xaxt="n")
+axis(side=1,at=seq(0,300,12))
+par(mfrow=c(1,2))
+acf(xm)
+pacf(xm) #la saisonnalité a bien disparu sauf sur la pacf
 
+pp.test(xm) #test de philippe perron, on rejette à 1% l'hypothèse que la série n'est pas stationnaire
 
-#au niveau acf : on voit qu'on rentre complètement dans l'itv de confiance pour l'acf au lag 17 (donc on prend q =16)
-#au niveau pacf : on rentre complètement à p = 4
+#au niveau acf : on voit qu'on rentre environ dans l'itv de confiance pour l'acf au lag 5 environ (donc on prend q = 4)
+#au niveau pacf : on rentre complètement à p = 2 mais on ressort à p = 12
+#dans les deux cas on ressort à p=12 même après différenciation ... 
 
-#donc il faudrait prendre un ARIMA(4, 0, 17)
+#deuxième saisonnalité?
+#xm_bis <- diff(xm, lag = 12)
+#plot(xm_bis)
+#par(mfrow=c(1,2))
+#acf(xm_bis)
+#pacf(xm_bis)
 
-arima4017 <- arima(xm, c(4,0,17))
+#on retire la moyenne de xm
+xm <- xm - mean(xm)
+plot(xm)
+acf(xm)
+pacf(xm)
 
-Box.test(arima4017$residuals, lag=6, type="Ljung-Box", fitdf=5) #
+arima_1 <- arima(xm, c(2,0,5))
+arima_1
+
+Box.test(arima_1$residuals, lag=6, type="Ljung-Box", fitdf=5)
 
 Qtests <- function(series, k, fitdf=0) {
   pvals <- apply(matrix(1:k), 1, FUN=function(l) {
@@ -44,8 +61,8 @@ Qtests <- function(series, k, fitdf=0) {
   })
   return(t(pvals))
 }
-Qtests(arima4017$residuals, 24, 5) #tests de LB pour les ordres 1 a 24
-round(Qtests(arima4017$residuals,24,fitdf=5),3)
+Qtests(arima_1$residuals, 24, 5) #tests de LB pour les ordres 1 a 24
+round(Qtests(arima_1$residuals,24,fitdf=5),3)
 
 signif <- function(estim){ #fonction de test des significations individuelles des coefficients
   coef <- estim$coef
@@ -55,49 +72,9 @@ signif <- function(estim){ #fonction de test des significations individuelles de
   return(rbind(coef,se,pval))
 }
 
-signif(arima4017)
+signif(arima_1)
 
 
 
 
-
-
-
-
-
-#avec une autre base ? 
-hydro <- read.csv('valeurs_mensuelles_hydrocarbures.csv', sep = ';')
-hydro <- hydro[2]
-
-hydro <- as.data.frame(data[-(1:3),])#on enlève les premières lignes qui ne sont pas des données
-indice_hydro <- as.data.frame(as.numeric(unlist(hydro)))
-
-serie_hydro <- zoo(indice_hydro[[1]]) #convertit le premier element de data en serie temporelle de type "zoo"
-T <- length(serie_hydro)
-serie_hydro <- serie_hydro[1:(T-4)]
-
-plot(serie_hydro)
-acf(serie_hydro)
-pacf(serie_hydro)
-
-pp.test(serie_hydro)
-
-
-
-#encore une autre base ? 
-elec <- read.csv('valeurs_mensuelles_elec.csv', sep = ';')
-elec <- elec[2]
-
-elec <- as.data.frame(data[-(1:3),])#on enlève les premières lignes qui ne sont pas des données
-indice_elec <- as.data.frame(as.numeric(unlist(hydro)))
-
-serie_elec <- zoo(indice_elec[[1]]) #convertit le premier element de data en serie temporelle de type "zoo"
-T <- length(serie_elec)
-serie_elec <- serie_elec[1:(T-4)]
-
-plot(serie_elec)
-acf(serie_elec)
-pacf(serie_elec)
-
-pp.test(serie_elec)
 
